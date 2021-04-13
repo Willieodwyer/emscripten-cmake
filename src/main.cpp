9a3599d50139c4b1d0a63f728d63fd0f9515fa48
Willieodwyer <willieod666@live.com>
2021-04-13 16:39:13 +0100

    Simplify code

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,7 @@
 #include <emscripten.h>
 #endif
 
-
-#include "fib.h"
 #include <vector>
-
-extern "C" {
-	int new_fib();
-	int next_val(int fib_instance);
-}
-
-auto instances = std::vector<Fib>();
-
-int next_val(int fib_instance) {
-	return instances[fib_instance].next();
-}
-
-int new_fib() {
-	instances.push_back(Fib());
-	return instances.size() - 1;
-}
-
-
 
 extern "C" int main(int argc, char** argv) {
   printf("hello, world!\n");
@@ -33,24 +13,16 @@ extern "C" int main(int argc, char** argv) {
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Surface *screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
 
-#ifdef TEST_SDL_LOCK_OPTS
-  EM_ASM("SDL.defaults.copyOnLock = false; SDL.defaults.discardOnLock = true; SDL.defaults.opaqueFrontBuffer = false;");
-#endif
 
   if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
+
   for (int i = 0; i < 256; i++) {
     for (int j = 0; j < 256; j++) {
-#ifdef TEST_SDL_LOCK_OPTS
-      // Alpha behaves like in the browser, so write proper opaque pixels.
-      int alpha = 255;
-#else
-      // To emulate native behavior with blitting to screen, alpha component is ignored. Test that it is so by outputting
-      // data (and testing that it does get discarded)
       int alpha = (i+j) % 255;
-#endif
       *((Uint32*)screen->pixels + i * 256 + j) = SDL_MapRGBA(screen->format, i, j, 255-i, alpha);
     }
   }
+
   if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
   SDL_Flip(screen); 
 
@@ -58,9 +30,6 @@ extern "C" int main(int argc, char** argv) {
   printf("and here is some text that should be HTML-friendly: amp: |&| double-quote: |\"| quote: |'| less-than, greater-than, html-like tags: |<cheez></cheez>|\nanother line.\n");
 
   SDL_Quit();
-
-  int fib1 = new_fib();
-  next_val(fib1);
 
   return 0;
 }
